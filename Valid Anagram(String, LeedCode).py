76afee1465c7388ddf4fdd0bf53d14aef3f92762
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2023-01-25 23:39:21 +0900

    Add files via upload

---
+++ Valid Anagram(String, LeedCode).py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        if len(s) == 0:
+            return False
+
+        list_s = list(s)
+        list_t = list(t)
+
+        list_s.sort()
+        list_t.sort()
+
+        for i in range(len(list_s)):
+            if list_s[i] != list_t[i]:
+                return False
+        return True
